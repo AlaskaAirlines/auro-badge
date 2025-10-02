ddcab906ad283a752317f31e33ef5e125e1344db
rmenner <ryan.menner@alaskaair.com>
2025-10-02 17:10:08 -0400

    feat!: upgrade package tooling to use auro-cli
    BREAKING CHANGE:
    Package exports have changed. To access the component class add /class to the end of the import. See "exports" in the package.json for more details.
    
    AlaskaAirlines/auro-cli#108
    AlaskaAirlines/auro-cli#109
    AlaskaAirlines/auro-cli#110
    AlaskaAirlines/auro-cli#111

--- test/auro-badge.test.js
+++ test/auro-badge.test.js
@@ -1,5 +1,5 @@
 import { expect, fixture, html } from "@open-wc/testing";
-import "../index.js";
+import "../src/registered";
 
 describe("auro-badge", () => {
   it("auro-badge is accessible", async () => {
